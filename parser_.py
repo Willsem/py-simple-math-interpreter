b241ce815dddfe7197651bb6c5983e56709fd5e1
Willsem <happymainer@gmail.com>
2022-04-15 11:54:59 +0300

    add bool expressions to parser

--- parser_.py
+++ parser_.py
@@ -2,79 +2,106 @@ from tokens import TokenType
 from nodes import *
 
 class Parser:
-	def __init__(self, tokens):
-		self.tokens = iter(tokens)
-		self.advance()
+    def __init__(self, tokens):
+        self.tokens = iter(tokens)
+        self.advance()
 
-	def raise_error(self):
-		raise Exception("Invalid syntax")
-	
-	def advance(self):
-		try:
-			self.current_token = next(self.tokens)
-		except StopIteration:
-			self.current_token = None
+    def raise_error(self):
+        raise Exception("Invalid syntax")
+    
+    def advance(self):
+        try:
+            self.current_token = next(self.tokens)
+        except StopIteration:
+            self.current_token = None
 
-	def parse(self):
-		if self.current_token == None:
-			return None
+    def parse(self):
+        if self.current_token == None:
+            return None
 
-		result = self.expr()
+        result = self.bool_expr()
 
-		if self.current_token != None:
-			self.raise_error()
+        if self.current_token != None:
+            self.raise_error()
 
-		return result
+        return result
 
-	def expr(self):
-		result = self.term()
+    def bool_expr(self):
+        result = self.expr()
 
-		while self.current_token != None and self.current_token.type in (TokenType.PLUS, TokenType.MINUS):
-			if self.current_token.type == TokenType.PLUS:
-				self.advance()
-				result = AddNode(result, self.term())
-			elif self.current_token.type == TokenType.MINUS:
-				self.advance()
-				result = SubtractNode(result, self.term())
+        while self.current_token != None and self.current_token.type in (
+                TokenType.MORE,
+                TokenType.MOREEQUAL,
+                TokenType.LESS,
+                TokenType.LESSEQUAL,
+                TokenType.EQUAL):
+            if self.current_token.type == TokenType.MORE:
+                self.advance()
+                result = MoreNode(result, self.expr())
+            elif self.current_token.type == TokenType.MOREEQUAL:
+                self.advance()
+                result = MoreEqualNode(result, self.expr())
+            elif self.current_token.type == TokenType.LESS:
+                self.advance()
+                result = LessNode(result, self.expr())
+            elif self.current_token.type == TokenType.LESSEQUAL:
+                self.advance()
+                result = LessEqualNode(result, self.expr())
+            elif self.current_token.type == TokenType.EQUAL:
+                self.advance()
+                result = EqualNode(result, self.expr())
 
-		return result
+        return result
 
-	def term(self):
-		result = self.factor()
+    def expr(self):
+        result = self.term()
 
-		while self.current_token != None and self.current_token.type in (TokenType.MULTIPLY, TokenType.DIVIDE):
-			if self.current_token.type == TokenType.MULTIPLY:
-				self.advance()
-				result = MultiplyNode(result, self.factor())
-			elif self.current_token.type == TokenType.DIVIDE:
-				self.advance()
-				result = DivideNode(result, self.factor())
-				
-		return result
+        while self.current_token != None and self.current_token.type in (TokenType.PLUS, TokenType.MINUS):
+            if self.current_token.type == TokenType.PLUS:
+                self.advance()
+                result = AddNode(result, self.term())
+            elif self.current_token.type == TokenType.MINUS:
+                self.advance()
+                result = SubtractNode(result, self.term())
 
-	def factor(self):
-		token = self.current_token
+        return result
 
-		if token.type == TokenType.LPAREN:
-			self.advance()
-			result = self.expr()
+    def term(self):
+        result = self.factor()
 
-			if self.current_token.type != TokenType.RPAREN:
-				self.raise_error()
-			
-			self.advance()
-			return result
+        while self.current_token != None and self.current_token.type in (TokenType.MULTIPLY, TokenType.DIVIDE):
+            if self.current_token.type == TokenType.MULTIPLY:
+                self.advance()
+                result = MultiplyNode(result, self.factor())
+            elif self.current_token.type == TokenType.DIVIDE:
+                self.advance()
+                result = DivideNode(result, self.factor())
+                
+        return result
 
-		elif token.type == TokenType.NUMBER:
-			self.advance()
-			return NumberNode(token.value)
+    def factor(self):
+        token = self.current_token
 
-		elif token.type == TokenType.PLUS:
-			self.advance()
-			return PlusNode(self.factor())
-		
-		elif token.type == TokenType.MINUS:
-			self.advance()
-			return MinusNode(self.factor())
-		
-		self.raise_error()
+        if token.type == TokenType.LPAREN:
+            self.advance()
+            result = self.expr()
+
+            if self.current_token.type != TokenType.RPAREN:
+                self.raise_error()
+            
+            self.advance()
+            return result
+
+        elif token.type == TokenType.NUMBER:
+            self.advance()
+            return NumberNode(token.value)
+
+        elif token.type == TokenType.PLUS:
+            self.advance()
+            return PlusNode(self.factor())
+        
+        elif token.type == TokenType.MINUS:
+            self.advance()
+            return MinusNode(self.factor())
+        
+        self.raise_error()
--- parser_test.py
+++ parser_test.py
@@ -5,79 +5,229 @@ from nodes import *
 
 class TestParser(unittest.TestCase):
 
-	def test_empty(self):
-		tokens = []
-		node = Parser(tokens).parse()
-		self.assertEqual(node, None)
+    def test_empty(self):
+        tokens = []
+        node = Parser(tokens).parse()
+        self.assertEqual(node, None)
 
-	def test_numbers(self):
-		tokens = [Token(TokenType.NUMBER, 51.2)]
-		node = Parser(tokens).parse()
-		self.assertEqual(node, NumberNode(51.2))
+    def test_numbers(self):
+        tokens = [Token(TokenType.NUMBER, 51.2)]
+        node = Parser(tokens).parse()
+        self.assertEqual(node, NumberNode(51.2))
 
-	def test_single_operations(self):
-		tokens = [
-			Token(TokenType.NUMBER, 27),
-			Token(TokenType.PLUS),
-			Token(TokenType.NUMBER, 14),
-		]
+    def test_single_operations(self):
+        tokens = [
+            Token(TokenType.NUMBER, 27),
+            Token(TokenType.PLUS),
+            Token(TokenType.NUMBER, 14),
+        ]
 
-		node = Parser(tokens).parse()
-		self.assertEqual(node, AddNode(NumberNode(27), NumberNode(14)))
-		
-		tokens = [
-			Token(TokenType.NUMBER, 27),
-			Token(TokenType.MINUS),
-			Token(TokenType.NUMBER, 14),
-		]
+        node = Parser(tokens).parse()
+        self.assertEqual(node, AddNode(NumberNode(27), NumberNode(14)))
+        
+        tokens = [
+            Token(TokenType.NUMBER, 27),
+            Token(TokenType.MINUS),
+            Token(TokenType.NUMBER, 14),
+        ]
 
-		node = Parser(tokens).parse()
-		self.assertEqual(node, SubtractNode(NumberNode(27), NumberNode(14)))
-			
-		tokens = [
-			Token(TokenType.NUMBER, 27),
-			Token(TokenType.MULTIPLY),
-			Token(TokenType.NUMBER, 14),
-		]
+        node = Parser(tokens).parse()
+        self.assertEqual(node, SubtractNode(NumberNode(27), NumberNode(14)))
+            
+        tokens = [
+            Token(TokenType.NUMBER, 27),
+            Token(TokenType.MULTIPLY),
+            Token(TokenType.NUMBER, 14),
+        ]
 
-		node = Parser(tokens).parse()
-		self.assertEqual(node, MultiplyNode(NumberNode(27), NumberNode(14)))
-			
-		tokens = [
-			Token(TokenType.NUMBER, 27),
-			Token(TokenType.DIVIDE),
-			Token(TokenType.NUMBER, 14),
-		]
+        node = Parser(tokens).parse()
+        self.assertEqual(node, MultiplyNode(NumberNode(27), NumberNode(14)))
+            
+        tokens = [
+            Token(TokenType.NUMBER, 27),
+            Token(TokenType.DIVIDE),
+            Token(TokenType.NUMBER, 14),
+        ]
 
-		node = Parser(tokens).parse()
-		self.assertEqual(node, DivideNode(NumberNode(27), NumberNode(14)))
+        node = Parser(tokens).parse()
+        self.assertEqual(node, DivideNode(NumberNode(27), NumberNode(14)))
 
-	def test_full_expression(self):
-		tokens = [
-			Token(TokenType.NUMBER, 27),
-			Token(TokenType.PLUS),
-			Token(TokenType.LPAREN),
-			Token(TokenType.NUMBER, 43),
-			Token(TokenType.DIVIDE),
-			Token(TokenType.NUMBER, 36),
-			Token(TokenType.MINUS),
-			Token(TokenType.NUMBER, 48),
-			Token(TokenType.RPAREN),
-			Token(TokenType.MULTIPLY),
-			Token(TokenType.NUMBER, 51),
-		]
+    def test_full_expression(self):
+        tokens = [
+            Token(TokenType.NUMBER, 27),
+            Token(TokenType.PLUS),
+            Token(TokenType.LPAREN),
+            Token(TokenType.NUMBER, 43),
+            Token(TokenType.DIVIDE),
+            Token(TokenType.NUMBER, 36),
+            Token(TokenType.MINUS),
+            Token(TokenType.NUMBER, 48),
+            Token(TokenType.RPAREN),
+            Token(TokenType.MULTIPLY),
+            Token(TokenType.NUMBER, 51),
+        ]
 
-		node = Parser(tokens).parse()
-		self.assertEqual(node, AddNode(
-			NumberNode(27),
-			MultiplyNode(
-				SubtractNode(
-					DivideNode(
-						NumberNode(43),
-						NumberNode(36)
-					),
-					NumberNode(48)
-				),
-				NumberNode(51)
-			)
-		))
+        node = Parser(tokens).parse()
+        self.assertEqual(node, AddNode(
+            NumberNode(27),
+            MultiplyNode(
+                SubtractNode(
+                    DivideNode(
+                        NumberNode(43),
+                        NumberNode(36)
+                    ),
+                    NumberNode(48)
+                ),
+                NumberNode(51)
+            )
+        ))
+
+    def test_bool_more_expression(self):
+        tokens = [
+            Token(TokenType.NUMBER, 27),
+            Token(TokenType.PLUS),
+            Token(TokenType.NUMBER, 43),
+            Token(TokenType.MORE),
+            Token(TokenType.LPAREN),
+            Token(TokenType.NUMBER, 12),
+            Token(TokenType.MINUS),
+            Token(TokenType.NUMBER, 4),
+            Token(TokenType.RPAREN),
+            Token(TokenType.MULTIPLY),
+            Token(TokenType.NUMBER, 2),
+        ]
+
+        node = Parser(tokens).parse()
+        self.assertEqual(node, MoreNode(
+            AddNode(
+                NumberNode(27),
+                NumberNode(43),
+            ),
+            MultiplyNode(
+                SubtractNode(
+                    NumberNode(12),
+                    NumberNode(4),
+                ),
+                NumberNode(2),
+            )
+        ))
+
+    def test_bool_more_equal_expression(self):
+        tokens = [
+            Token(TokenType.NUMBER, 27),
+            Token(TokenType.PLUS),
+            Token(TokenType.NUMBER, 43),
+            Token(TokenType.MOREEQUAL),
+            Token(TokenType.LPAREN),
+            Token(TokenType.NUMBER, 12),
+            Token(TokenType.MINUS),
+            Token(TokenType.NUMBER, 4),
+            Token(TokenType.RPAREN),
+            Token(TokenType.MULTIPLY),
+            Token(TokenType.NUMBER, 2),
+        ]
+
+        node = Parser(tokens).parse()
+        self.assertEqual(node, MoreEqualNode(
+            AddNode(
+                NumberNode(27),
+                NumberNode(43),
+            ),
+            MultiplyNode(
+                SubtractNode(
+                    NumberNode(12),
+                    NumberNode(4),
+                ),
+                NumberNode(2),
+            )
+        ))
+
+    def test_bool_less_expression(self):
+        tokens = [
+            Token(TokenType.NUMBER, 27),
+            Token(TokenType.PLUS),
+            Token(TokenType.NUMBER, 43),
+            Token(TokenType.LESS),
+            Token(TokenType.LPAREN),
+            Token(TokenType.NUMBER, 12),
+            Token(TokenType.MINUS),
+            Token(TokenType.NUMBER, 4),
+            Token(TokenType.RPAREN),
+            Token(TokenType.MULTIPLY),
+            Token(TokenType.NUMBER, 2),
+        ]
+
+        node = Parser(tokens).parse()
+        self.assertEqual(node, LessNode(
+            AddNode(
+                NumberNode(27),
+                NumberNode(43),
+            ),
+            MultiplyNode(
+                SubtractNode(
+                    NumberNode(12),
+                    NumberNode(4),
+                ),
+                NumberNode(2),
+            )
+        ))
+
+    def test_bool_less_equal_expression(self):
+        tokens = [
+            Token(TokenType.NUMBER, 27),
+            Token(TokenType.PLUS),
+            Token(TokenType.NUMBER, 43),
+            Token(TokenType.LESSEQUAL),
+            Token(TokenType.LPAREN),
+            Token(TokenType.NUMBER, 12),
+            Token(TokenType.MINUS),
+            Token(TokenType.NUMBER, 4),
+            Token(TokenType.RPAREN),
+            Token(TokenType.MULTIPLY),
+            Token(TokenType.NUMBER, 2),
+        ]
+
+        node = Parser(tokens).parse()
+        self.assertEqual(node, LessEqualNode(
+            AddNode(
+                NumberNode(27),
+                NumberNode(43),
+            ),
+            MultiplyNode(
+                SubtractNode(
+                    NumberNode(12),
+                    NumberNode(4),
+                ),
+                NumberNode(2),
+            )
+        ))
+
+    def test_bool_equal_expression(self):
+        tokens = [
+            Token(TokenType.NUMBER, 27),
+            Token(TokenType.PLUS),
+            Token(TokenType.NUMBER, 43),
+            Token(TokenType.EQUAL),
+            Token(TokenType.LPAREN),
+            Token(TokenType.NUMBER, 12),
+            Token(TokenType.MINUS),
+            Token(TokenType.NUMBER, 4),
+            Token(TokenType.RPAREN),
+            Token(TokenType.MULTIPLY),
+            Token(TokenType.NUMBER, 2),
+        ]
+
+        node = Parser(tokens).parse()
+        self.assertEqual(node, EqualNode(
+            AddNode(
+                NumberNode(27),
+                NumberNode(43),
+            ),
+            MultiplyNode(
+                SubtractNode(
+                    NumberNode(12),
+                    NumberNode(4),
+                ),
+                NumberNode(2),
+            )
+        ))
