b7fddf22b867a1061e543ec10c61db7bcc0b09fe
Willsem <happymainer@gmail.com>
2022-04-15 11:20:57 +0300

    add support of bool tokens

--- lexer.py
+++ lexer.py
@@ -4,60 +4,84 @@ WHITESPACE = ' \n\t'
 DIGITS = '0123456789'
 
 class Lexer:
-	def __init__(self, text):
-		self.text = iter(text)
-		self.advance()
+    def __init__(self, text):
+        self.text = iter(text)
+        self.advance()
 
-	def advance(self):
-		try:
-			self.current_char = next(self.text)
-		except StopIteration:
-			self.current_char = None
+    def advance(self):
+        try:
+            self.current_char = next(self.text)
+        except StopIteration:
+            self.current_char = None
 
-	def generate_tokens(self):
-		while self.current_char != None:
-			if self.current_char in WHITESPACE:
-				self.advance()
-			elif self.current_char == '.' or self.current_char in DIGITS:
-				yield self.generate_number()
-			elif self.current_char == '+':
-				self.advance()
-				yield Token(TokenType.PLUS)
-			elif self.current_char == '-':
-				self.advance()
-				yield Token(TokenType.MINUS)
-			elif self.current_char == '*':
-				self.advance()
-				yield Token(TokenType.MULTIPLY)
-			elif self.current_char == '/':
-				self.advance()
-				yield Token(TokenType.DIVIDE)
-			elif self.current_char == '(':
-				self.advance()
-				yield Token(TokenType.LPAREN)
-			elif self.current_char == ')':
-				self.advance()
-				yield Token(TokenType.RPAREN)
-			else:
-				raise Exception(f"Illegal character '{self.current_char}'")
+    def generate_tokens(self):
+        while self.current_char != None:
+            if self.current_char in WHITESPACE:
+                self.advance()
+            elif self.current_char == '.' or self.current_char in DIGITS:
+                yield self.generate_number()
+            elif self.current_char == '+':
+                self.advance()
+                yield Token(TokenType.PLUS)
+            elif self.current_char == '-':
+                self.advance()
+                yield Token(TokenType.MINUS)
+            elif self.current_char == '*':
+                self.advance()
+                yield Token(TokenType.MULTIPLY)
+            elif self.current_char == '/':
+                self.advance()
+                yield Token(TokenType.DIVIDE)
+            elif self.current_char == '(':
+                self.advance()
+                yield Token(TokenType.LPAREN)
+            elif self.current_char == ')':
+                self.advance()
+                yield Token(TokenType.RPAREN)
+            elif self.current_char == '<' or self.current_char == '>' or self.current_char == '=':
+                yield self.generate_bool()
+            else:
+                raise Exception(f"Illegal character '{self.current_char}'")
 
-	def generate_number(self):
-		decimal_point_count = 0
-		number_str = self.current_char
-		self.advance()
+    def generate_number(self):
+        decimal_point_count = 0
+        number_str = self.current_char
+        self.advance()
 
-		while self.current_char != None and (self.current_char == '.' or self.current_char in DIGITS):
-			if self.current_char == '.':
-				decimal_point_count += 1
-				if decimal_point_count > 1:
-					break
-			
-			number_str += self.current_char
-			self.advance()
+        while self.current_char != None and (self.current_char == '.' or self.current_char in DIGITS):
+            if self.current_char == '.':
+                decimal_point_count += 1
+                if decimal_point_count > 1:
+                    break
+            
+            number_str += self.current_char
+            self.advance()
 
-		if number_str.startswith('.'):
-			number_str = '0' + number_str
-		if number_str.endswith('.'):
-			number_str += '0'
+        if number_str.startswith('.'):
+            number_str = '0' + number_str
+        if number_str.endswith('.'):
+            number_str += '0'
 
-		return Token(TokenType.NUMBER, float(number_str))
+        return Token(TokenType.NUMBER, float(number_str))
+
+    def generate_bool(self):
+        symbol = self.current_char
+        self.advance()
+
+        if self.current_char == '=':
+            self.advance()
+            if symbol == '<':
+                return Token(TokenType.LESSEQUAL)
+            elif symbol == '>':
+                return Token(TokenType.MOREEQUAL)
+            elif symbol == '=':
+                return Token(TokenType.EQUAL)
+        else:
+            if symbol == '<':
+                return Token(TokenType.LESS)
+            elif symbol == '>':
+                return Token(TokenType.MORE)
+            elif symbol == '=':
+                raise Exception(f"Illegal character '='")
+
+        raise Exception(f"Unknown error with parsing: {symbol + self.current_char}")
--- lexer_test.py
+++ lexer_test.py
@@ -3,53 +3,153 @@ from tokens import Token, TokenType
 from lexer import Lexer
 
 class TestLexer(unittest.TestCase):
-	
-	def test_empty(self):
-		tokens = list(Lexer("").generate_tokens())
-		self.assertEqual(tokens, [])
-	
-	def test_whitespace(self):
-		tokens = list(Lexer(" \t\n  \t\t\n\n").generate_tokens())
-		self.assertEqual(tokens, [])
+    
+    def test_empty(self):
+        tokens = list(Lexer("").generate_tokens())
+        self.assertEqual(tokens, [])
+    
+    def test_whitespace(self):
+        tokens = list(Lexer(" \t\n  \t\t\n\n").generate_tokens())
+        self.assertEqual(tokens, [])
 
-	def test_numbers(self):
-		tokens = list(Lexer("123 123.456 123. .456 .").generate_tokens())
-		self.assertEqual(tokens, [
-			Token(TokenType.NUMBER, 123.000),
-			Token(TokenType.NUMBER, 123.456),
-			Token(TokenType.NUMBER, 123.000),
-			Token(TokenType.NUMBER, 000.456),
-			Token(TokenType.NUMBER, 000.000),
-		])
-	
-	def test_operators(self):
-		tokens = list(Lexer("+-*/").generate_tokens())
-		self.assertEqual(tokens, [
-			Token(TokenType.PLUS),
-			Token(TokenType.MINUS),
-			Token(TokenType.MULTIPLY),
-			Token(TokenType.DIVIDE),
-		])
+    def test_numbers(self):
+        tokens = list(Lexer("123 123.456 123. .456 .").generate_tokens())
+        self.assertEqual(tokens, [
+            Token(TokenType.NUMBER, 123.000),
+            Token(TokenType.NUMBER, 123.456),
+            Token(TokenType.NUMBER, 123.000),
+            Token(TokenType.NUMBER, 000.456),
+            Token(TokenType.NUMBER, 000.000),
+        ])
+    
+    def test_operators(self):
+        tokens = list(Lexer("+-*/").generate_tokens())
+        self.assertEqual(tokens, [
+            Token(TokenType.PLUS),
+            Token(TokenType.MINUS),
+            Token(TokenType.MULTIPLY),
+            Token(TokenType.DIVIDE),
+        ])
 
-	def test_parens(self):
-		tokens = list(Lexer("()").generate_tokens())
-		self.assertEqual(tokens, [
-			Token(TokenType.LPAREN),
-			Token(TokenType.RPAREN),
-		])
-	
-	def test_all(self):
-		tokens = list(Lexer("27 + (43 / 36 - 48) * 51").generate_tokens())
-		self.assertEqual(tokens, [
-			Token(TokenType.NUMBER, 27),
-			Token(TokenType.PLUS),
-			Token(TokenType.LPAREN),
-			Token(TokenType.NUMBER, 43),
-			Token(TokenType.DIVIDE),
-			Token(TokenType.NUMBER, 36),
-			Token(TokenType.MINUS),
-			Token(TokenType.NUMBER, 48),
-			Token(TokenType.RPAREN),
-			Token(TokenType.MULTIPLY),
-			Token(TokenType.NUMBER, 51),
-		])
+    def test_parens(self):
+        tokens = list(Lexer("()").generate_tokens())
+        self.assertEqual(tokens, [
+            Token(TokenType.LPAREN),
+            Token(TokenType.RPAREN),
+        ])
+    
+    def test_all(self):
+        tokens = list(Lexer("27 + (43 / 36 - 48) * 51").generate_tokens())
+        self.assertEqual(tokens, [
+            Token(TokenType.NUMBER, 27),
+            Token(TokenType.PLUS),
+            Token(TokenType.LPAREN),
+            Token(TokenType.NUMBER, 43),
+            Token(TokenType.DIVIDE),
+            Token(TokenType.NUMBER, 36),
+            Token(TokenType.MINUS),
+            Token(TokenType.NUMBER, 48),
+            Token(TokenType.RPAREN),
+            Token(TokenType.MULTIPLY),
+            Token(TokenType.NUMBER, 51),
+        ])
+
+    def test_less(self):
+        tokens = list(Lexer("12 < 45").generate_tokens())
+        self.assertEqual(tokens, [
+            Token(TokenType.NUMBER, 12),
+            Token(TokenType.LESS),
+            Token(TokenType.NUMBER, 45),
+        ])
+
+    def test_less_equal(self):
+        tokens = list(Lexer("12 <= 45").generate_tokens())
+        self.assertEqual(tokens, [
+            Token(TokenType.NUMBER, 12),
+            Token(TokenType.LESSEQUAL),
+            Token(TokenType.NUMBER, 45),
+        ])
+
+    def test_more(self):
+        tokens = list(Lexer("12 > 45").generate_tokens())
+        self.assertEqual(tokens, [
+            Token(TokenType.NUMBER, 12),
+            Token(TokenType.MORE),
+            Token(TokenType.NUMBER, 45),
+        ])
+
+    def test_more_equal(self):
+        tokens = list(Lexer("12 >= 45").generate_tokens())
+        self.assertEqual(tokens, [
+            Token(TokenType.NUMBER, 12),
+            Token(TokenType.MOREEQUAL),
+            Token(TokenType.NUMBER, 45),
+        ])
+
+    def test_equal(self):
+        tokens = list(Lexer("12 == 45").generate_tokens())
+        self.assertEqual(tokens, [
+            Token(TokenType.NUMBER, 12),
+            Token(TokenType.EQUAL),
+            Token(TokenType.NUMBER, 45),
+        ])
+
+    def test_less_complex(self):
+        tokens = list(Lexer("12 + 56 < 45 * 1").generate_tokens())
+        self.assertEqual(tokens, [
+            Token(TokenType.NUMBER, 12),
+            Token(TokenType.PLUS),
+            Token(TokenType.NUMBER, 56),
+            Token(TokenType.LESS),
+            Token(TokenType.NUMBER, 45),
+            Token(TokenType.MULTIPLY),
+            Token(TokenType.NUMBER, 1),
+        ])
+
+    def test_less_equal_complex(self):
+        tokens = list(Lexer("12 + 56 <= 45 * 1").generate_tokens())
+        self.assertEqual(tokens, [
+            Token(TokenType.NUMBER, 12),
+            Token(TokenType.PLUS),
+            Token(TokenType.NUMBER, 56),
+            Token(TokenType.LESSEQUAL),
+            Token(TokenType.NUMBER, 45),
+            Token(TokenType.MULTIPLY),
+            Token(TokenType.NUMBER, 1),
+        ])
+
+    def test_more_complex(self):
+        tokens = list(Lexer("12 + 56 > 45 * 1").generate_tokens())
+        self.assertEqual(tokens, [
+            Token(TokenType.NUMBER, 12),
+            Token(TokenType.PLUS),
+            Token(TokenType.NUMBER, 56),
+            Token(TokenType.MORE),
+            Token(TokenType.NUMBER, 45),
+            Token(TokenType.MULTIPLY),
+            Token(TokenType.NUMBER, 1),
+        ])
+
+    def test_more_equal_complex(self):
+        tokens = list(Lexer("12 + 56 >= 45 * 1").generate_tokens())
+        self.assertEqual(tokens, [
+            Token(TokenType.NUMBER, 12),
+            Token(TokenType.PLUS),
+            Token(TokenType.NUMBER, 56),
+            Token(TokenType.MOREEQUAL),
+            Token(TokenType.NUMBER, 45),
+            Token(TokenType.MULTIPLY),
+            Token(TokenType.NUMBER, 1),
+        ])
+
+    def test_equal_complex(self):
+        tokens = list(Lexer("12 + 56 == 45 * 1").generate_tokens())
+        self.assertEqual(tokens, [
+            Token(TokenType.NUMBER, 12),
+            Token(TokenType.PLUS),
+            Token(TokenType.NUMBER, 56),
+            Token(TokenType.EQUAL),
+            Token(TokenType.NUMBER, 45),
+            Token(TokenType.MULTIPLY),
+            Token(TokenType.NUMBER, 1),
+        ])
